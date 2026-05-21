e77e67a9e8dc66723c1e8c5ffb142368d28df296
embacak <embacak@tsn.at>
2026-05-21 16:51:30 +0200

    db

---
+++ Rest.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+db = sqlite3.connect("restaurant.db")
+
+cursor = db.cursor()
+
+cursor.execute("PRAGMA foreign_keys = ON;")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Gast (
+        Gast_ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+        Name TEXT, 
+        Telefonnummer TEXT
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Bestellung (
+        Bestell_ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+        Gast_ID INTEGER, 
+        Datum DATE,
+        FOREIGN KEY (Gast_ID) REFERENCES Gast(Gast_ID)
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Tisch (
+        Tisch_ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+        Plaetze INTEGER
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Rechnung (
+        Rechnung_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        BestellID INTEGER,
+        Gesamtpreis FLOAT,
+        Datum DATE,
+        FOREIGN KEY (BestellID) REFERENCES Bestellung(Bestell_ID), 
+)
+""")
+
+
+db.commit()
+cursor.close()
+db.close()
